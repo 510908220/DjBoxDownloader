3a043b45f2fd3a8655712bce10147a98ae1180a1
呼忠忠 <510908220@qq.com>
2015-05-05 19:46:36 +0800

    修改

---
+++ dj.py
@@ -0,0 +1,75 @@
+# -*- encoding: utf-8 -*-
+
+from urllib.parse import urljoin
+import sys
+import os
+import requests
+from bs4 import BeautifulSoup
+
+import song
+import util
+
+MOST_DOWNLOAD_PAGE_URL = "http://www.djyule.com/downPH.htm"
+BASE_URL = "http://www.djyule.com"
+MUSIC_STORE = "d:\\music"
+
+def _get(song_url):
+	response = requests.get(song_url)
+	if response.status_code == 200:
+		response.encoding = "utf-8"
+		return response.text
+
+
+def get_page_urls():
+	def get_page_count():
+		html = _get(MOST_DOWNLOAD_PAGE_URL)
+		if html:
+			soup = BeautifulSoup(html)
+			div_page = soup.find('div', attrs={"class": "List_Page3"})
+			if div_page:
+				a_items = div_page.find_all("a")
+				if len(a_items) > 0:
+					href = a_items[-1]["href"]
+					return int(href.split(".")[0].split("_")[1])  # downPH_50.htm
+
+	page_urls = []
+	count = get_page_count()
+
+	for page_index in range(1, 50 + 1):
+		if page_index == 1:
+			url = MOST_DOWNLOAD_PAGE_URL
+		else:
+			url = "http://www.djyule.com/downPH_%d.htm" % page_index
+		page_urls.append(url)
+	return page_urls
+
+
+def get_song_urls(page_url):
+	html = _get(page_url)
+	song_urls = []
+	if html:
+		soup = BeautifulSoup(html)
+		form = soup.find('form', attrs={"id": "frmList"})
+		# a href="/DJ/120696.htm" target="_blankDJPlay"
+		a_items = form.find_all("a", {"target": "_blankDJPlay"})
+		for item in a_items:
+			song_url = urljoin(BASE_URL, item["href"])
+			song_urls.append(song_url)
+	return song_urls
+
+# 命令行控制下载全部，部分，等
+def main():
+	page_urls = get_page_urls()
+	song_url = get_song_urls(page_urls[0])[0]
+	song_ = song.Song(song_url)
+	d_path = os.path.join(MUSIC_STORE, song_.name + ".mp3")
+	dn = util.Downloader(song_.download_url,d_path)
+	dn.start()
+	while not dn.finished:
+		util.progress_bar(dn.progress)
+	util.progress_bar(100)
+
+
+
+if __name__ == "__main__":
+	sys.exit(main())
--- util.py
+++ util.py
@@ -8,73 +8,69 @@
 import queue
 import threading
 import requests
-import time
-import os
+import sys
 
 
 def worker(download_url, progress, local_path):
-    def update_progress(total_size, recived_size):
-        percent = int((recived_size / total_size) * 100)
-        if progress.empty():
-            progress.put_nowait(percent)
+	def update_progress(total_size, recived_size):
+		percent = int((recived_size / total_size) * 100)
+		if progress.empty():
+			progress.put_nowait(percent)
 
-    r = requests.get(download_url, stream=True)
-    total_size = int(r.headers['Content-Length'])
-    print("total_size is", total_size)
+	r = requests.get(download_url, stream=True)
+	total_size = int(r.headers['Content-Length'])
 
-    with open(local_path, 'wb') as f:
-        recived_size = 0
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                recived_size += len(chunk)
-                update_progress(total_size, recived_size)
-                f.write(chunk)
-                f.flush()
+	with open(local_path, 'wb') as f:
+		recived_size = 0
+		for chunk in r.iter_content(chunk_size=1024):
+			if chunk:  # filter out keep-alive new chunks
+				recived_size += len(chunk)
+				update_progress(total_size, recived_size)
+				f.write(chunk)
+				f.flush()
+
 
 def progress_bar(procent):
-    os.system('clear')
-    buffer = "进度:%{0} {1}".format(procent, procent * "+")
-    print(buffer)
+	"""
+	控制台显示进度条
+	:param procent: 下载完成百分比
+	:return:
+	"""
+	buffer = "%{0} {1}".format(procent, int(procent / 5) * "#")
+	sys.stdout.write("\r%s" % buffer)
+	sys.stdout.flush()
+
 
 class Downloader(object):
-    def __init__(self, download_url, local_path):
-        self._download_url = download_url
-        self._local_path = local_path
-        self._progress = queue.Queue(1)
-        self._last_percent = 0
-        self._thread = None
+	def __init__(self, download_url, local_path):
+		self._download_url = download_url
+		self._local_path = local_path
+		self._progress = queue.Queue(1)
+		self._last_percent = 0
+		self._thread = None
 
-    @property
-    def progress(self):
-        """
-        :return:下载进度
-        """
-        percent = 0
-        if self._progress.full():
-            percent = self._progress.get_nowait()
-            self._last_percent = percent
-        else:
-            percent = self._last_percent
-        return percent
+	@property
+	def progress(self):
+		"""
+		:return:下载进度
+		"""
+		percent = 0
+		if self._progress.full():
+			percent = self._progress.get_nowait()
+			self._last_percent = percent
+		else:
+			percent = self._last_percent
+		return percent
 
 
-    @property
-    def finished(self):
-        return self._thread.is_alive()
+	@property
+	def finished(self):
+		return self._thread.is_alive()
 
-    def start(self):
-        self._thread = threading.Thread(target=worker, args=(self._download_url, self._progress, self._local_path))
-        self._thread.daemon = True
-        self._thread.start()
-
-if __name__ == "__main__":
-    url = "http://a64-5.jyw8.com:8080/up20100924-1/DJ%CE%CA%C7%E9-2010%C7%BF%BA%B4%B6%AF%B8%D0%B3%B5%D4%D8%C8%AB%D6%D0%CE%C4Club.mp3?up20100924-1/DJ%CE%CA%C7%E9-2010%C7%BF%BA%B4%B6%AF%B8%D0%B3%B5%D4%D8%C8%AB%D6%D0%CE%C4C8ub.mp3?50886618630126432167401881173198672747524287525838"
-    path  = "d:\\aa.mp3"
-    dw = Downloader(url, path)
-    dw.start()
-    while  dw.finished:
-        progress_bar(dw.progress)
-        time.sleep(2)
+	def start(self):
+		self._thread = threading.Thread(target=worker, args=(self._download_url, self._progress, self._local_path))
+		self._thread.daemon = True
+		self._thread.start()
 
 
 
